43bb434cb331a9dfd4631b418330b3e1f618fbfd
Reshmaa123 <143435835+Reshmaa123@users.noreply.github.com>
2023-09-17 18:43:01 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import './App.css';
+//import ChooseUs from './components/ChooseUs';
+import Editdata from './components/Editdata';
+import Navigation from './components/Navigation';
+import PostData from './components/PostData';
+import Routing from './components/Routing';
+import Head from './components/head';
+import GridExampleInverted from './components/foot';
+import About from './components/About';
+import Getdata from './components/Getdata';
+import { Callback } from './components/Callback';
+function App()
+{
+        return(
+        <div className="App">
+          <Callback/>
+</div>
+    )
+}
+export default App
